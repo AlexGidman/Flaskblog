1f613a7525a31d96d09604fe8a4040c1e707015a
Alex Gidman <alexgidman@outlook.com>
2020-09-28 17:24:00 +0100

    registration now working

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, url_for, redirect, flash, request
+from werkzeug.security import generate_password_hash, check_password_hash 
+import sqlite3
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev'
+conn = sqlite3.connect("site.db", check_same_thread=False,)
+db = conn.cursor()
+#CREATE TABLES...
 
 @app.route("/")
 @app.route("/home")
@@ -14,11 +20,29 @@ def about():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-            
-        # If registration success, log user in and redirect to homepage
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # Error Check
+        check = db.execute("SELECT username FROM user WHERE username = ?;", [username])
+        if (check.fetchall()):
+            flash("Username already exists!")
+            return render_template("register.html")
+        if (len(password) < 10):
+            flash("Password must be 10 characters or more!")
+            return render_template("register.html")
+        
+        # Store into database 
+        pwhash = generate_password_hash(password)
+        db.execute("INSERT INTO user (username, password) VALUES (?, ?)", (username, pwhash))
+        conn.commit()
+        
+        # Log user in and redirect to homepage
+        # Session Id...
         return redirect(url_for("home"))
-    return render_template("register.html")
+    else: 
+        return render_template("register.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    #(check_password_hash(pwdhash, password)
     return render_template("login.html")
