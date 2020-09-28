afee61f7d6b9b3b7f8ac20491a307327fbf4c93f
Alex Gidman <alexgidman@outlook.com>
2020-09-28 15:03:36 +0100

    initial Flask app

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, flash, request
 
 app = Flask(__name__)
 
@@ -10,3 +10,15 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+            
+        # If registration success, log user in and redirect to homepage
+        return redirect(url_for("home"))
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
