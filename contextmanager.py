8e5ff71845275de93a8cd31450af8b7706749e2f
Alex Gidman <alexgidman@outlook.com>
2020-09-29 13:38:28 +0100

    added missing contextmanager.py & .gitignore file

---
+++ contextmanager.py
@@ -0,0 +1,16 @@
+import sqlite3
+from contextlib import contextmanager
+
+@contextmanager
+def SQL(query):
+    conn = sqlite3.connect("site.db", check_same_thread=False)
+    db = conn.cursor()
+    db.execute(query)
+    rows = db.fetchall()
+    yield rows
+    conn.commit()
+    conn.close()
+
+with SQL("SELECT * from user") as rows:
+    for row in rows:
+        print(row[1])
