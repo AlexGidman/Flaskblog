4babefdd50c00f18b2fa9677070de56a0a10d3f9
Alex Gidman <alexgidman@outlook.com>
2020-10-01 17:18:21 +0100

    added REST API for time at profile location

--- profile.py
+++ profile.py
@@ -2,12 +2,17 @@ from flask import Blueprint, render_template, session, request, redirect, url_fo
 from decorators import login_required
 from contextmanagers import SQL
 import sqlite3
+import requests
 
 ### SQLite3 
 conn = sqlite3.connect("site.db", check_same_thread=False,)
 db = conn.cursor()
 
 profile = Blueprint("profile", __name__, static_folder="static", template_folder="templates")
+
+BASE = "http://worldtimeapi.org/api/timezone/"
+response = requests.get(BASE)
+locations = response.json()
 
 @profile.route("/home")
 @profile.route("/", methods=["GET", "POST"])
@@ -18,7 +23,24 @@ def home():
     if request.method == "POST":
         if request.form.get("button") == "edit":
             print(request.form.get("button"))
-            return render_template("edit.html", user_profile=user_profile)
+            return render_template("edit.html", 
+                                    user_profile=user_profile,
+                                    locations=locations)
+        elif request.form.get("button") == "save":
+            print(request.form.get("button"))
+            user_id = request.form.get("id")
+            first = request.form.get("first")
+            last = request.form.get("last")
+            profession = request.form.get("profession")
+            interests = request.form.get("interests")
+            location = request.form.get("location")
+            if not user_profile:
+                db.execute("INSERT INTO profile (user_id, first, last, profession, interests,location) VALUES (?,?,?,?,?,?)",(user_id, first, last, profession, interests,location))
+                conn.commit()
+            else:
+                db.execute("UPDATE profile SET first=?, last=?, profession=?, interests=?,location=? WHERE user_id=?",(first, last, profession, interests,location, user_id))
+                conn.commit()
+            return redirect(url_for('profile.home'))    
         elif request.form.get("button") == "deleteposts":
             print(request.form.get("button"))
             db.execute("DELETE FROM posts WHERE user_id=?", (session["id"],))
@@ -32,4 +54,12 @@ def home():
             return redirect(url_for("logout")) 
         else:
             flash("Unsuccessful")
-    return render_template("profile.html", user_profile=user_profile)
+    # Get time based on location
+    time = ""
+    if (user_profile):
+        BASE = "http://worldtimeapi.org/api/timezone/"
+        response = requests.get(BASE + user_profile[6])
+        time = response.json()['datetime']
+    return render_template("profile.html", 
+                            user_profile=user_profile,
+                            time=time[11:19])
